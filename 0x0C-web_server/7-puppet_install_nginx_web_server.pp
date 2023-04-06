2d38aab6bd46773c0035903d2a58f6cf23e525ea
Nwadike Joseph <jcyoung.mjc44@gmail.com>
2023-04-06 15:54:49 -0700

    web server corrections

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -15,7 +15,7 @@ exec {'Hello':
   provider => shell,
 }
 
-exec {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/blog.smartempire.com\/;\\n\\t}/" /etc/nginx/sites-available/default':
+exec {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/https://youtu.be/dPAw4opzN9g\/;\\n\\t}/" /etc/nginx/sites-available/default':
   provider => shell,
 }
 
